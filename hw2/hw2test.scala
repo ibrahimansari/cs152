6a0c18cc351fec7398d61bf25d940227f3766388
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-02-06 02:32:21 -0800

    update

--- hw2/hw2test.scala
+++ hw2/hw2test.scala
@@ -1,4 +1,6 @@
 object hw2test extends App {
   println(hw2.lcs("Mary had a little lamb", "Its fleece was white as snow"))
+  println(hw2.subs("Amy"))
+  println(hw2.onebits(13))
   // your other test cases
 }
