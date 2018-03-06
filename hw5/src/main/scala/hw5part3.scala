e5d4edd71c57680b7a39cc7604fb1a15d9f44e0e
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-03-06 03:18:56 -0800

    hw5

--- hw5/src/main/scala/hw5part3.scala
+++ hw5/src/main/scala/hw5part3.scala
@@ -5,16 +5,16 @@ object hw5part3 {
     class SimpleLanguageParser extends JavaTokenParsers {
         def expr: Parser[Any] = term ~ opt(("+" | "-") ~ expr)
         def term: Parser[Any] = factor ~ opt(("*" | "/" ) ~ term)
-        def factor: Parser[Any] = wholeNumber | "(" ~ expr ~ ")"
-        def valdef: Parser[Any] = ident 
-        def eval(x : Any) : Int = x match {
-            case a ~ Some("+" ~ b) => eval(a) + eval(b)
-            case a ~ Some("-" ~ b) => eval(a) - eval(b)
-            case a ~ Some("*" ~ b) => eval(a) * eval(b)
-            case a ~ Some("/" ~ b) => eval(a) / eval(b)
-            case a ~ None => eval(a)
+        def factor: Parser[Any] = wholeNumber| ident | "(" ~ expr ~ ")"
+        def valdef: Parser[Any] = "val" ~ ident ~ "=" ~ expr
+        def eval(x : Any, symbols: Map[String, Int]) : Int = x match {
+            case a ~ Some("+" ~ b) => eval(a,symbols) + eval(b,symbols)
+            case a ~ Some("-" ~ b) => eval(a,symbols) - eval(b,symbols)
+            case a ~ Some("*" ~ b) => eval(a,symbols) * eval(b,symbols)
+            case a ~ Some("/" ~ b) => eval(a,symbols) / eval(b,symbols)
+            case a ~ None => eval(a,symbols)
             case a : String => Integer.parseInt(a)
-            case "(" ~ a ~ ")" => eval(a)
+            case "(" ~ a ~ ")" => eval(a,symbols)
         }
     }
 }
