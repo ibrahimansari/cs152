811f6d9110b98a0cbcfc4c01746f4c443d4a27ff
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-03-13 02:27:45 -0700

    added hw6part1

---
+++ hw6/src/main/scala/hw6part1.scala
@@ -0,0 +1,59 @@
+import java.io._
+import scala.util.parsing.combinator._
+
+object hw6part1 {
+
+  val parser = new SimpleLanguageParser
+  parser.parseAll(parser.prog, new InputStreamReader(System.in)) match {
+    case parser.Success(result, next) => println(result.eval)
+    case _ => println("Error")
+  }
+
+  class Expr
+
+  case class Number(value: Int) extends Expr
+
+  case class Variable(name: String) extends Expr
+
+  case class Operator(left: Expr, right: Expr, f: (Int, Int) => Int) extends Expr
+
+  case class Def(name: String, expr: Expr)
+
+  case class Prog(defs: List[Def], expr: Expr) {
+    def eval: Int = {
+      val m : Map[String, Expr] = defs.map(x => (x.name, x.expr)).toMap
+      eval2(expr, m)
+    }
+
+    def eval2(expr: Expr, map: Map[String, Expr]): Int =
+      expr match {
+        case Number(num) => num
+        case Variable(name) => eval2(map(name), map)
+        case Operator(left, right, f) => f(eval2(left, map), eval2(right, map))
+      }
+  }
+
+  class SimpleLanguageParser extends JavaTokenParsers {
+    def expr: Parser[Expr] = (term ~ rep(("+" | "-") ~ term)) ^^ {
+      case a ~ lst => (a /: lst) {
+        case (x, "+" ~ y) => Operator(x, y, _ + _)
+        case (x, "-" ~ y) => Operator(x, y, _ - _)
+      }
+    }
+
+    def term: Parser[Expr] = (factor ~ rep(("*" | "/") ~ factor)) ^^ {
+      case a ~ lst => (a /: lst) {
+        case (x, "*" ~ y) => Operator(x, y, _ * _)
+        case (x, "/" ~ y) => Operator(x, y, _ / _)
+      }
+    }
+
+    def factor: Parser[Expr] = ident ^^ (x => Variable(x)) | wholeNumber ^^ (x => Number(x.toInt)) | "(" ~> expr <~ ")"
+
+    def valdef: Parser[Def] = ("val" ~> ident <~ "=") ~ expr <~ ";" ^^ { case s ~ e => Def(s, e) }
+
+    def prog: Parser[Prog] = rep(valdef) ~ expr ^^ { case s ~ e => Prog(s, e) }
+
+  }
+
+}
---
+++ hw6/src/main/scala/hw6part2.scala
@@ -0,0 +1,22 @@
+import java.io._
+import scala.util.parsing.combinator._
+
+object hw6part2 {
+
+  val parser = new ListParser
+  parser.parseAll(parser.list, new InputStreamReader(System.in)) match {
+    case parser.Success(result, next) => println(result)
+    case _ => println("Error")
+  }
+
+  class ListItem
+  case class Number(value: Int) extends ListItem
+  case class Contents(values: Int*) extends ListItem
+  case class Append(left: Number, right: Contents) extends ListItem
+
+  class ListParser extends JavaTokenParsers {
+    def list: Parser[ListItem] =
+
+  }
+
+}
