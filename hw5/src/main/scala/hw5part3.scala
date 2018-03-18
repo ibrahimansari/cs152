0d8ef4a5976c999d9155753ed10faa39dbac77b6
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-03-17 18:52:42 -0700

    hw 5 fix

--- hw5/src/main/scala/hw5part3.scala
+++ hw5/src/main/scala/hw5part3.scala
@@ -1,20 +1,25 @@
 import scala.util.parsing.combinator._
 
 object hw5part3 {
-
     class SimpleLanguageParser extends JavaTokenParsers {
         def expr: Parser[Any] = term ~ opt(("+" | "-") ~ expr)
         def term: Parser[Any] = factor ~ opt(("*" | "/" ) ~ term)
-        def factor: Parser[Any] = wholeNumber| ident | "(" ~ expr ~ ")"
-        def valdef: Parser[Any] = "val" ~ ident ~ "=" ~ expr
+        def factor: Parser[Any] = wholeNumber | ident | "(" ~ expr ~ ")"
+
         def eval(x : Any, symbols: Map[String, Int]) : Int = x match {
-            case a ~ Some("+" ~ b) => eval(a,symbols) + eval(b,symbols)
-            case a ~ Some("-" ~ b) => eval(a,symbols) - eval(b,symbols)
-            case a ~ Some("*" ~ b) => eval(a,symbols) * eval(b,symbols)
-            case a ~ Some("/" ~ b) => eval(a,symbols) / eval(b,symbols)
-            case a ~ None => eval(a,symbols)
-            case a : String => Integer.parseInt(a)
-            case "(" ~ a ~ ")" => eval(a,symbols)
+            case a ~ Some("+" ~ b) => eval(a, symbols) + eval(b, symbols)
+            case a ~ Some("-" ~ b) => eval(a, symbols) - eval(b, symbols)
+            case a ~ Some("*" ~ b) => eval(a, symbols) * eval(b, symbols)
+            case a ~ Some("/" ~ b) => eval(a, symbols) / eval(b, symbols)
+            case a ~ None => eval(a, symbols)
+            case a : String => {
+                try {
+                    Integer.parseInt(a)
+                } catch {
+                    case e: NumberFormatException => symbols(a)
+                }
+            }
+            case "(" ~ a ~ ")" => eval(a, symbols)
         }
     }
 }
