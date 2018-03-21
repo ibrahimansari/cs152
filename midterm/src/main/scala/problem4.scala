3c3fe721d92317cf6f972749a947e14a41471db0
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-03-21 13:03:15 -0700

    10 min commit

--- midterm/src/main/scala/problem4.scala
+++ midterm/src/main/scala/problem4.scala
@@ -2,16 +2,14 @@ import scala.util.parsing.combinator._
 
 object problem4 extends App {
    class SimpleLanguageParser extends JavaTokenParsers {    
-     def expr: Parser[Boolean] = (term ~ opt(("+" | "-") ~ expr)) ^^ {
+     def expr: Parser[Boolean] = (term ~ opt(("||") ~ expr)) ^^ {
        case a ~ None => a
-       case a ~ Some("+" ~ b) => a + b
-       case a ~ Some("-" ~ b) => a - b
+       case a ~ Some("||" ~ b) => a || b
      } 
     
-     def term: Parser[Boolean] = (factor ~ opt(("*" | "/") ~ term)) ^^ {
+     def term: Parser[Boolean] = (factor ~ opt(("&&") ~ term)) ^^ {
        case a ~ None => a
-       case a ~ Some("*" ~ b) => a * b
-       case a ~ Some("/" ~ b) => a / b
+       case a ~ Some("&&" ~ b) => a && b
      }
      
      def factor: Parser[Boolean] = expr | "(" ~> expr <~ ")"
