17ee13045b86158d3f2d763e0bb7181cb017e791
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-03-21 12:32:42 -0700

    10 min commit

--- midterm/src/main/scala/problem1.scala
+++ midterm/src/main/scala/problem1.scala
@@ -1,5 +1,11 @@
 object problem1 extends App {
-  def diffs(lst: List[Int]) = ...
+  def diffs(lst: List[Int]) = {
+    def helper(lst : List[Int], dif : List[Int]) : List[Int] = {
+      if(lst.tail.tail.isEmpty) dif :+ lst.head - lst.tail.head
+      else helper(lst.tail, dif :+ lst.head - lst.tail.head)
+    }
+    helper(lst, List())
+  }
 
   println(diffs(List(3, 1, 4, 1, 5, 9, 2, 6)))
 }
---
+++ midterm/src/main/scala/test.sc
@@ -0,0 +1,13 @@
+def diffs(lst : List[Int]) = {
+    val seed: (List[Int], Option[Int]) = (Nil, None)
+    val foldResult = (lst :\ seed)((element, state) =>
+    ...
+    )
+    foldResult._1
+}
+
+/*
+   Your diagram here...
+
+ */
+println(diffs(List(1, 7, 2, 9)))
