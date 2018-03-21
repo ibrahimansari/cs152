b5088bfdd4509b4a5ef55d33aa69b793c137bb36
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-03-21 13:00:34 -0700

    10 min commit

--- midterm/src/main/scala/problem3.scala
+++ midterm/src/main/scala/problem3.scala
@@ -1,5 +1,7 @@
 object problem3 extends App {
-  def allCombinations(as: List[Int], bs: List[Int]) = ...
-  
+  def allCombinations(as: List[Int], bs: List[Int]) = {
+    as.flatMap(x => bs.map(y => (x.toString + y.toString).toInt))
+  }
+
   println(allCombinations(List(1, 2), List(3, 4, 5)))
 }
