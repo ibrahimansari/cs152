312c2b73e0d354be0f2c4b308c8259d93132e9ef
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-05-16 09:59:17 -0700

    10 min update

--- final/problem1.rkt
+++ final/problem1.rkt
@@ -1,4 +1,8 @@
-(define (invertCycle cycle) ...)
+(define (invertCycle cycle)
+  (foldl (λ (newElement partialResult) (+ newElement (* 10 partialResult))) 0 cycle)
+  )
 
-(define (invert permutation) ...)
+(define (invert permutation)
+  (map (λ (x) (invertCycle x)) permutation)
+  )
         
