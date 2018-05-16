dc65ddcd7a8cd3602fde88485dfbe5dd929d0751
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-05-16 11:12:33 -0700

    10 min update

--- final/problem4/src/main/scala/Table.scala
+++ final/problem4/src/main/scala/Table.scala
@@ -1,5 +1,9 @@
-class Table[K,V](entries: List[(K,V)] = List()) { // TODO: Type variance
-  def get(k: K) = . . . // TODO: Implementation
+class Table[K,-V](entries: List[(K,V)] = List()) { // TODO: Type variance
+  def get(k: K) = entries.foreach(K => K._1 match {
+    case None => Nil
+    case Some => if(k == K._1) K._2
+  })
+  // TODO: Implementation
   // TODO: The put method doesn't quite work. Make the appropriate
   // changes.
   def put(k: K, v: V): Table[K, V] = new Table((k, v) :: entries)
