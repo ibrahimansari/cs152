c432c3b1107ee04a78776972fcfa560461a212f2
Ibrahim Ansari <ibrahim2221916@gmail.com>
2018-05-16 10:15:01 -0700

    10 min update

--- final/problem1.rkt
+++ final/problem1.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (define (invertCycle cycle)
-  (flatten (foldr (λ (new partial) (append (list partial) (list new))) (list (list-ref cycle 0)) cycle))
+  (flatten (foldr (λ (new partial) (append (list partial) (list new))) (list (list-ref cycle 0)) (list-tail cycle 1)))
   )
 
 (define (invert permutation)
